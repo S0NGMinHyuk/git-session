c0a1938339effdf631e2e20f64ee18cc637e23ce
MinHyuk SONG <88926519+S0NGMinHyuk@users.noreply.github.com>
2025-02-07 12:59:09 +0900

    2 (#6)
    * hello
    
    * 333
    
    * 444444444
    
    * 이즈리얼 너프안
    
    * 2번기능 완료
    
    * 2번기능 완료222

--- JavaClass.java
+++ JavaClass.java
@@ -2,6 +2,9 @@ public class JavaClass {
   1+1;
   2+2;
   4+4;
+  22222222222222;
+  asdfasdfasdfasddfasdfasdf;
+  2222222222222;
   1111111111111111111;
   asdfasdfsdfasdf;
   111111111111111111111;
